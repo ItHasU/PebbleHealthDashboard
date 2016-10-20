27acb85113570049f7405333610da37e1087c371
Matthieu Aubry <ithasu@gmail.com>
2016-10-20 07:10:41 +0200

    Removed chart gradient

--- index.js
+++ index.js
@@ -168,18 +168,6 @@ $(function () {
             },
             plotOptions: {
                 area: {
-                    fillColor: {
-                        linearGradient: {
-                            x1: 0,
-                            y1: 0,
-                            x2: 0,
-                            y2: 1
-                        },
-                        stops: [
-                            [0, Highcharts.getOptions().colors[0]],
-                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                        ]
-                    },
                     marker: {
                         radius: 2
                     },
